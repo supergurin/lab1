fe5d3e3a85945d0d46227e06314e7dcb4e72ca23
supergurin <illya_gurin1998@ukr.net>
2016-05-20 08:24:44 +0300

    Вірш

---
+++ Virsh.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main ()
+
+{
+	setlocale(LC_ALL, "Russian");
+	printf("\n\Я помню чудное мгновенье\nПередо мной явилась ты,\nКак мимолетное виденье,\nКак гения чистой красоты\n\n");
+	
+	return 0;
+}
