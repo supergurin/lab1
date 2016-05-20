948b3614686575010311422ab918f78f871f43d1
supergurin <illya_gurin1998@ukr.net>
2016-05-20 08:49:50 +0300

    3.c

---
+++ riv3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x;
+	printf("Vvedite x:\nx = ");
+	scanf("%d", &x);
+	printf("x = %d\n", x);
+	printf("x = %o\n", x);
+	printf("x = %x\n", x);
+	printf("x = %u\n", x);
+	
+	return 0;
+}
